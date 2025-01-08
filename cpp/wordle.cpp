2a983a1211aafc66a7bc6647f504ba2b3c9ca946
Na-pewno-nie-Szymon <ciastkojablkowe@gmail.com>
2025-01-08 20:57:18 +0100

    dzialaja nowe brancze

---
+++ cpp/wordle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void dataLoader(){
+    
+}
+
+bool guess_checker(string player_guess){
+    return false
+}
+
+void game_loop(){
+
+}
+
+int main(){
+    return 0
+}
